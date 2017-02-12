e32bd4d59edc5c7879e8e3b7dc3a134e61db73d4
Steven Cable <steven.cable12@gmail.com>
2017-02-11 16:29:53 -1000

    scss, css, and route up

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 const gulp = require('gulp');
 const scss = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const connect = require('gulp-connect');
 
-gulp.task('scss', () =>{
+gulp.task('scss', () => {
   return gulp.src('./scss/*.scss')
-  .pipe(scss())
-  .pipe(gulp.dest('./public/css'));
+    .pipe(scss())
+    .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('browserSync', ()=>{
-  return browserSync.init(['./public'], {
-    server: {
-      baseDir: './public'
-    }
-  });
-});
+// gulp.task('connect', function(){
+//   connect.server({
+//     root: './public',
+//     livereload: true
+//   })
+// })
 
-gulp.task('watch', () =>{
+gulp.task('watch', ['live-reload'], ()=>{
   gulp.watch('./scss/**/*.scss', ['scss']);
 });
 
-gulp.task('default', ['scss', 'watch', 
-  'browserSync']);
+gulp.task('default', ['scss', 'watch']);
+
+gulp.task('live-reload', function(){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
+});
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bp = require('body-parser');
 const indexRoute = require('./routes/index.js');
-// const methodOverride = require('method-override');
-//need to require any time we add a new route file
 const PORT = process.env.PORT || 3000;
 const app = express();
 const db = require('./models');
 
+app.use(express.static('public'));
+app.use('/gallery', indexRoute);
+app.use(bp.urlencoded({extended: true}));
 
 const hbs = handlebars.create({
   extname: '.hbs',
@@ -17,11 +18,7 @@ const hbs = handlebars.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-app.use(express.static('public'))
-app.use(bp.urlencoded({extended: true}));
-app.use('/', indexRoute);
-//app.use(methodOverride('_method'));
-//add app.use for other js files through their routes!
+
 app.listen(PORT, ()=>{
   db.sequelize.sync();
   console.log('you are now in the matrix via port, ', PORT);
